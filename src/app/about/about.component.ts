1ff18aa5d5a948ee65c6e94c61349a18aa0bbbf9
may_do <may.tdo15@gmail.com>
2022-03-20 21:22:57 -0500

    add buddy imgs and display about screen

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,13 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
-export class AboutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class AboutComponent {
+  constructor() {}
 }
