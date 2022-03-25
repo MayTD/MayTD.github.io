dcef43b3aaf51de49b678373c317668c53cfd4cd
may_do <may.tdo15@gmail.com>
2022-03-25 09:18:27 -0500

    add hoa and reconstruct projects

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,18 +11,33 @@ export class HomeComponent {
 
   cardsList: Card[] = [
     {
+      title: 'Arcade Typing',
+      description:
+        'A simple typing game that measures accuracy, speed, and time to test your typing skills. Made with Django framework.',
+      src: '../../../assets/img/typing.png',
+      alt: 'Photo of Arcade Typing app',
+    },
+    {
       title: 'DoMo Macro Pad',
       description:
         'A custom 3x3 keyboard that supports a rotary encoder and OLED display. Made with KiCad for PCB design and integrated with QMK Firmware in C for more customizability.',
       src: '../../../assets/img/keyboard.png',
       alt: 'Photo of a keyboard',
     },
+
     {
-      title: 'Arcade Typing',
+      title: 'SimpleHOA',
       description:
-        'A simple typing game that measures accuracy, speed, and time to test your typing skills. Made with Django framework.',
-      src: '../../../assets/img/typing.png',
-      alt: 'Photo of Arcade Typing app',
+        "A home owner association management system with the self-serve tools to maintain manager's workspace. The project was a web application made with MySQL, Express, React, and Node.js. and Bootstrap.",
+      src: '../../../assets/img/hoa.png',
+      alt: 'Photo of simpleHOA app',
+    },
+    {
+      title: 'reConctruct',
+      description:
+        'An app that connects contractors and construction workers to share supplies to reduce waste. The project was a web application made with MySQL, Express, React, and Node.js.',
+      src: '../../../assets/img/reConstruct.png',
+      alt: 'Photo of reConstruct app',
     },
     {
       title: 'Rising Waters',
